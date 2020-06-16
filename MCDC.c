c35ab137cbbab55f2e259b15a0a015790bcf19d1
Cou01000111 <courange.c@gmail.com>
2020-06-16 11:48:09 +0900

    MIN,MAXマクロを修正

--- MCDC.c
+++ MCDC.c
@@ -28,8 +28,8 @@
 
 #define DEBUG_SWITCH true
 
-#define MAX(a,b) (a > b ? a : b)
-#define MIN(a,b) (a < b ? a : b)
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
 char yesNo;
 
